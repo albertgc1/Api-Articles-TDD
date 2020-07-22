b30e191477ef9213792680ec970a6677801773d7
albertgc <beto.17cv@gmail.com>
2020-07-22 19:57:37 -0400

    models --all for articles and categories with relationships is doit

---
+++ app/Models/Article.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $fillable = [
+    	'title', 'slug', 'content', 'category_id', 'user_id'
+    ];
+
+    public function user()
+    {
+    	return $this->belongsTo(App\Models\User::class);
+    }
+
+    public function category()
+    {
+    	return $this->belongsTo(App\Models\Category::class);
+    }
+}
